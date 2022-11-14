25acab00469645c2a619c46fdead2ad3bde0c13a
Gerard Rodes <gerardrodesvidal@gmail.com>
2022-11-14 09:18:07 +0100

    Add executor

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from datetime import date
 # orjson is a much faster deserializer, it provides us a free speed up
 # but I am not going to use it as it feels unfair for the purposes of this benchmark
 import json
+from concurrent import futures
+import os
 
 
 fieldnames = [
@@ -24,27 +26,42 @@ def write_csv(records):
         writer.writerows(records)
 
 
-async def worker(queue: asyncio.Queue):
-    while True:
-        records = await queue.get()
-        write_csv(records)
-        queue.task_done()
+def load_json(data: bytes):
+    result = dict({
+        'records_valid': 0,
+        'records_invalid': 0,
+        'records': []
+    })
 
+    fr = io.BytesIO(data)
+    for record in fr.readlines():
+        try:
+            item = json.loads(record)
+            # pick only required fields to reduce serialization cost on thread communication
+            result['records'].append({k:item[k] for k in fieldnames})
+            result['records_valid'] += 1
+        except Exception:
+            result['records_invalid'] += 1
+
+    return result
 
 @tornado.web.stream_request_body
 class DataReceiverHandler(tornado.web.RequestHandler):
-    def initialize(self, queue: asyncio.Queue):
-        self.queue = queue
+    def initialize(self, exec: futures.ProcessPoolExecutor, loop: asyncio.AbstractEventLoop):
+        self.exec = exec
+        self.loop = loop
         self.bytes_len = 0
         self.prev_tail = b''
         self.records = []
         self.records_valid = 0
         self.records_invalid = 0
+        self.futures = []
 
     def data_received(self, chunk):
         self.bytes_len += len(chunk)
         head, tail = chunk.rsplit(b'\n', 1)
-        self.load_json(self.prev_tail + head)
+        self.futures.append(self.loop.run_in_executor(self.exec, load_json, self.prev_tail + head))
+        # self.load_json(self.prev_tail + head)
         self.prev_tail = tail
 
     def load_json(self, data: bytes):
@@ -56,9 +73,21 @@ class DataReceiverHandler(tornado.web.RequestHandler):
             except Exception:
                 self.records_invalid += 1
 
-    def post(self):
-        self.load_json(self.prev_tail)
-        self.queue.put_nowait(self.records)
+    async def post(self):
+        self.futures.append(self.loop.run_in_executor(self.exec, load_json, self.prev_tail))
+
+        records_valid = 0
+        records_invalid = 0
+        records = []
+        # as_completed will change the futures order
+        # but we don't care if we write the lines in a different order
+        for future in asyncio.as_completed(self.futures):
+            result = await future
+            records_valid += result["records_valid"]
+            records_invalid += result["records_invalid"]
+            records += result['records']
+
+        write_csv(records)
         self.write({
             'result': {
                 'status': 'ok',
@@ -74,20 +103,18 @@ class DataReceiverHandler(tornado.web.RequestHandler):
         })
 
 sockets = tornado.netutil.bind_sockets(8888)
-tornado.process.fork_processes(num_processes=None)
+tornado.process.fork_processes(num_processes=6)
 async def run():
-    queue = asyncio.Queue()
+    executor = futures.ProcessPoolExecutor(max_workers=3)
     application = tornado.web.Application([
         (r"/", DataReceiverHandler, {
-            'queue': queue,
+            'exec': executor,
+            'loop': asyncio.get_running_loop(),
         }),
     ], debug=bool(sys.flags.debug))
     tornado.httpserver.HTTPServer(application).add_sockets(sockets)
-    task = asyncio.create_task(worker(queue))
     await asyncio.Event().wait()
-    task.cancel()
-    await asyncio.wait([task])
-
+    executor.shutdown()
 
 if __name__ == "__main__":
     asyncio.run(run())
